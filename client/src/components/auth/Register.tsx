39e94149405ea282bd5adbe38922f47aa402b8d9
Dat Ngo <ngohavandat93ndc@gmail.com>
2019-12-25 00:11:39 +0700

    Updated Register Page

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,10 +1,83 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (value: any) => {
+    setFormData({ ...formData, [value.target.name]: value.target.value })
+  }
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else { console.log(formData); }
+  }
+
   return (
-    <div>
-      <p>Register</p>
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={(e: any) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            required
+            onChange={(value: any) => onChange(value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+            value={email}
+            onChange={(value: any) => onChange(value)}
+          />
+          <small className="form-text"
+          >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength={6}
+            value={password}
+            required
+            onChange={(value: any) => onChange(value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength={6}
+            value={password2}
+            required
+            onChange={(value: any) => onChange(value)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </Fragment>
   )
 }
 
