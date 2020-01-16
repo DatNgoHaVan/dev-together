b5738cc1b5c233bea72068779555163f03417052
Dat Ngo <ngohavandat93ndc@gmail.com>
2020-01-17 00:03:00 +0700

    Updated Dashboard for Profile User

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import { Spinner } from '../layout/Spinner';
+import { StoreRootState } from '../../reducers/root/root-reducer';
+import { Link } from 'react-router-dom';
 
-const Dashboard = (props: any) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}: any) => {
+
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+    loading && profile === null ? (<Spinner />) : (
+      <Fragment>
+        <h1 className="large text-primary"></h1>
+        <p className="lead">
+          <i className="fas fa-user"></i>
+          Welcome {user && user.name}
+        </p>
+        {profile !== null ? (
+          <Fragment>has</Fragment>
+        ) : (
+            <Fragment>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to='/create-profile' className='btn btn-primary my-1'>
+                Create Profile
+              </Link>
+            </Fragment>)
+        }
+      </Fragment>
+    )
+  );
+};
 
 Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
+const mapStateToProps = (state: StoreRootState, ownProps: any) => ({
+  auth: state.auth,
+  profile: state.profile
+});
 
-export default Dashboard
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
