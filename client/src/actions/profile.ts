4f72362e7a2f3c9556d2d3ced392e62f58da2ab0
Dat Ngo <ngohavandat93ndc@gmail.com>
2020-01-19 18:28:36 +0700

    Updated submit event for Create Profile

--- client/src/actions/profile.ts
+++ client/src/actions/profile.ts
@@ -16,7 +16,47 @@ export const getCurrentProfile = () => {
       dispatch({
         type: 'PROFILE_ERROR',
         data: { msg: err.response.statusText, status: err.response.status }
-      })
+      });
+    }
+  }
+}
+
+// Create or update profile
+export const createProfile = (formData: any, history: any, edit: boolean = false) => {
+  return async (dispatch: Dispatch<IActionProfile | any>) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+
+      const res = await axios.post('/api/profile', formData, config);
+
+      dispatch({
+        type: 'GET_PROFILE',
+        data: res.data
+      });
+
+      dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+      if (!edit) {
+        history.push('/dashboard');
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error: any) => {
+          dispatch(setAlert(error.msg, 'danger', 3000))
+        }
+        );
+      }
+
+      dispatch({
+        type: 'PROFILE_ERROR',
+        data: { msg: err.response.statusText, status: err.response.status }
+      });
     }
   }
 }
--- client/src/components/profile-forms/CreateProfile.tsx
+++ client/src/components/profile-forms/CreateProfile.tsx
@@ -1,7 +1,10 @@
 import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = () => {
+const CreateProfile = ({ createProfile, history }: any) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -34,7 +37,12 @@ const CreateProfile = () => {
     linkedin
   } = formData;
 
-  const onChange = (e: any) => setFormData({ ...formData, [e.target.name]: e.target.value })
+  const onChange = (e: any) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
 
   return (
     <Fragment>
@@ -46,7 +54,7 @@ const CreateProfile = () => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e: any) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -83,9 +91,8 @@ const CreateProfile = () => {
         </div>
         <div className="form-group">
           <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={e => onChange(e)} />
-          <small className="form-text"
-          >Please use comma separated values (eg.
-                  HTML,CSS,JavaScript,PHP)
+          <small className="form-text">
+            Please use comma separated values (eg.HTML,CSS,JavaScript,PHP)
           </small>
         </div>
         <div className="form-group">
@@ -144,11 +151,11 @@ const CreateProfile = () => {
         <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
       </form>
     </Fragment>
-  )
-}
+  );
+};
 
 CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-}
-
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile));
