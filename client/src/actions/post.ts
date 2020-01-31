a194cacba2e70fd7452bb5167325daa5de8868a0
Dat Ngo <ngohavandat93ndc@gmail.com>
2020-01-31 23:16:56 +0700

    Updated action for Post

--- client/src/actions/post.ts
+++ client/src/actions/post.ts
@@ -67,7 +67,7 @@ export const removeLike = (postId: string) => {
 export const deletePost = (postId: string) => {
   return async (dispatch: Dispatch<IActionPost>) => {
     try {
-      const res = await axios.delete(`/api/posts/${postId}`);
+      await axios.delete(`/api/posts/${postId}`);
 
       dispatch({
         type: 'DELETE_POST',
@@ -75,6 +75,105 @@ export const deletePost = (postId: string) => {
       });
 
       dispatch(setAlert('Post Removed', 'success', 4000));
+
+    } catch (err) {
+      dispatch({
+        type: 'POST_ERROR',
+        data: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+};
+
+// Add post
+export const addPost = (formData: any) => {
+  return async (dispatch: Dispatch<IActionPost>) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post(`/api/posts`, formData, config);
+
+      dispatch({
+        type: 'ADD_POST',
+        data: res.data
+      });
+
+      dispatch(setAlert('Post Created', 'success', 4000));
+
+    } catch (err) {
+      dispatch({
+        type: 'POST_ERROR',
+        data: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+};
+
+// Get posts
+export const getPost = (postId: string) => {
+  return async (dispatch: Dispatch<IActionPost>) => {
+    try {
+      const res = await axios.get(`/api/posts/${postId}`);
+
+      dispatch({
+        type: 'GET_POST',
+        data: res.data
+      });
+
+    } catch (err) {
+      dispatch({
+        type: 'POST_ERROR',
+        data: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+};
+
+// Add comment
+export const addComment = (postId: string, formData: any) => {
+  return async (dispatch: Dispatch<IActionPost>) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+      dispatch({
+        type: 'ADD_COMMENT',
+        data: res.data
+      });
+
+      dispatch(setAlert('Comment Added', 'success', 4000));
+
+    } catch (err) {
+      dispatch({
+        type: 'POST_ERROR',
+        data: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+};
+
+// Delete comment
+export const deleteComment = (postId: string, commentId: string) => {
+  return async (dispatch: Dispatch<IActionPost>) => {
+
+    try {
+      const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+      dispatch({
+        type: 'DELETE_COMMENT',
+        data: commentId
+      });
+
+      dispatch(setAlert('Comment Removed', 'success', 4000));
 
     } catch (err) {
       dispatch({
