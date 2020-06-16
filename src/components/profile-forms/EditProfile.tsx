e7bb2d3b0125bf17c57ceb17d4a8ff2cec5c9b5c
datngo <ngohavandat93ndc@gmail.com>
2020-06-16 11:00:28 +0700

    Added config

--- src/components/profile-forms/EditProfile.tsx
+++ src/components/profile-forms/EditProfile.tsx
@@ -75,8 +75,8 @@ const EditProfile = ({
         Create Your Profile
       </h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Let's get some information to make your
-        profile stand out
+        <i className="fas fa-user"></i>
+        {`Let's get some information to make your profile stand out`}
       </p>
       <small>* = required field</small>
       <form className="form" onSubmit={(e) => onSubmit(e)}>
